2bd56f5c7a640121749f75cfbb8808291f03a8b8
Eric Morales <ericdaniel3103@gmail.com>
2022-01-06 12:42:09 -0600

    Creando el modal

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,12 @@ function App() {
   const [presupuesto, setPresupuesto] = useState(0);
   const [isValidPresupuesto, setIsValidPresupuesto] = useState(false);
 
+  const [modal, setModal] = useState(false);
+
+  const handleNuevoGasto = () => {
+    setModal(true);
+  };
+
   return (
     <div>
       <Header
@@ -17,9 +23,15 @@ function App() {
 
       {isValidPresupuesto && (
         <div className="nuevo-gasto">
-          <img src={IconoNuevoGasto} alt="icono nuevo gasto" />
+          <img
+            onClick={handleNuevoGasto}
+            src={IconoNuevoGasto}
+            alt="icono nuevo gasto"
+          />
         </div>
       )}
+
+      {modal && <p>Desde modal</p>}
     </div>
   );
 }
