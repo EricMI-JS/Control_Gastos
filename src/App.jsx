9d6d5bd7f80470f26ab0bb2341c84563a30bb79c
Eric Morales <ericdaniel3103@gmail.com>
2022-01-08 18:25:54 -0600

    Validando los gastos

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,8 @@ function App() {
   const [modal, setModal] = useState(false);
   const [animarModal, setAnimarModal] = useState(false);
 
+  const [gastos, setGastos] = useState([]);
+
   const handleNuevoGasto = () => {
     setModal(true);
 
@@ -17,6 +19,10 @@ function App() {
       setAnimarModal(true);
     }, 250);
   };
+
+  const guardarGasto = gasto => {
+    console.log(gasto);
+  }
 
   return (
     <div>
@@ -42,6 +48,7 @@ function App() {
           setModal={setModal}
           animarModal={animarModal}
           setAnimarModal={setAnimarModal}
+          guardarGasto={guardarGasto}
         />
       )}
     </div>
